9e7d0fae9b0a96d8fdc361bb06dbc8009b811762
aidenchia <aidenchia95@gmail.com>
2020-04-18 17:42:33 +0800

    python server able to instantiate k

---
+++ contracts/server.py
@@ -0,0 +1,38 @@
+import json
+
+from flask import Flask, render_template
+
+from web3 import Web3
+
+app = Flask(__name__)
+
+ganache_url = "http://127.0.0.1:7545"
+web3 = Web3(Web3.HTTPProvider(ganache_url))
+
+# Compiled by Remix
+abi = json.loads(open('abi.json', 'r').read())
+bytecode = json.loads(open('bytecode.json', 'r').read())['object']
+
+# Set default account
+web3.eth.defaultAccount = web3.eth.accounts[0]
+
+# Create smart contract object
+CF = web3.eth.contract(abi=abi, bytecode=bytecode)
+
+# Deploy smart contract
+tx_hash = CF.constructor().transact()
+tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
+
+print(tx_receipt)
+
+# Instantiate the smart contract at specified address
+contract = web3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+
+
+
+
+
+
+
+
+
