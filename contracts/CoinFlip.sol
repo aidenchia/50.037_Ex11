d9b523b89630c0bb01ea1cb6f620c798c4db3dde
aidenchia <aidenchia95@gmail.com>
2020-04-19 15:07:49 +0800

    adding ERC-20 standard interface such as transfer function

--- contracts/CoinFlip.sol
+++ contracts/CoinFlip.sol
@@ -2,6 +2,27 @@ pragma solidity ^0.5.0;
 
 
 contract CoinFlip {
+
+	// ERC-20 standard
+	string public name = "CoinFlip";
+	string public symbol = "CF";
+	uint256 public totalSupply;
+	mapping(address => uint256) public balanceOf;
+	mapping(address => mapping(address => uint256)) public allowance;
+
+	event Transfer (
+		address indexed _from,
+		address indexed _to,
+		uint256 _value
+	);
+
+	event Approval (
+		address indexed _owner,
+		address indexed _spender,
+		uint256 _value
+	);
+
+	// State variables for coin flip functions
 	address payable public player1;
 	address payable public player2;
 	address payable public winner;
@@ -12,9 +33,23 @@ contract CoinFlip {
 	uint256 public expiration;
 	uint public pot;
 	uint minBet;
-	
+
 	constructor() public {
 	    minBet = 10 wei;
+	    // Creator of contract will hold 1000000 tokens
+	    balanceOf[msg.sender] = 1000000;
+	    totalSupply = 1000000;
+	}
+
+	function transfer(address _to, uint256 _value) public returns (bool success) {
+		require(balanceOf[msg.sender] >= _value);
+
+		balanceOf[msg.sender] -= _value;
+		balanceOf[msg.sender] += _value;
+
+		emit Transfer(msg.sender, _to, _value);
+
+		return true;
 	}
 	
 	function getPot() public view returns(uint) {
